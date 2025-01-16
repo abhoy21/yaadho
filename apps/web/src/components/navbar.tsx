d5a4c01765db637360dbb7abcce272c0b326b5e3
Abhoy Sarkar <sarkar.ab07@gmail.com>
2025-01-16 10:26:11 +0530

    sessionprovider added

--- apps/web/src/components/navbar.tsx
+++ apps/web/src/components/navbar.tsx
@@ -1,52 +1,55 @@
 import { Button } from "@repo/ui/button";
+import { SessionProvider } from "next-auth/react";
 import Link from "next/link";
 import Logo from "./logo";
 import { SignInButton } from "./sign-in-button";
 
 export default function Navbar(): React.JSX.Element {
   return (
-    <nav className="sticky left-0 right-0 top-0 z-50 mx-auto max-w-7xl rounded-lg bg-white/30 px-4 shadow-md backdrop-blur-md sm:px-6 lg:px-8">
-      <div className="flex h-16 items-center justify-between">
-        <Logo />
+    <SessionProvider>
+      <nav className="sticky left-0 right-0 top-0 z-50 mx-auto max-w-7xl rounded-lg bg-white/30 px-4 shadow-md backdrop-blur-md sm:px-6 lg:px-8">
+        <div className="flex h-16 items-center justify-between">
+          <Logo />
 
-        <div className="hidden items-center space-x-8 md:flex">
-          <Link
-            className="text-sm text-black duration-300 ease-in-out hover:text-gray-700"
-            href="/home"
-          >
-            Features
-          </Link>
-          <Link
-            className="text-sm text-black duration-300 ease-in-out hover:text-gray-700"
-            href="/home"
-          >
-            Solutions
-          </Link>
-          <Link
-            className="text-sm text-black duration-300 ease-in-out hover:text-gray-700"
-            href="/home"
-          >
-            Pricing
-          </Link>
-          <Link
-            className="text-sm text-black duration-300 ease-in-out hover:text-gray-700"
-            href="/home"
-          >
-            About
-          </Link>
+          <div className="hidden items-center space-x-8 md:flex">
+            <Link
+              className="text-sm text-black duration-300 ease-in-out hover:text-gray-700"
+              href="/home"
+            >
+              Features
+            </Link>
+            <Link
+              className="text-sm text-black duration-300 ease-in-out hover:text-gray-700"
+              href="/home"
+            >
+              Solutions
+            </Link>
+            <Link
+              className="text-sm text-black duration-300 ease-in-out hover:text-gray-700"
+              href="/home"
+            >
+              Pricing
+            </Link>
+            <Link
+              className="text-sm text-black duration-300 ease-in-out hover:text-gray-700"
+              href="/home"
+            >
+              About
+            </Link>
+          </div>
+
+          <div className="flex items-center space-x-4">
+            <SignInButton />
+
+            <Button
+              className="hover:bg-primary/45"
+              size="md"
+              text="Get Demo"
+              variant="outline"
+            />
+          </div>
         </div>
-
-        <div className="flex items-center space-x-4">
-          <SignInButton />
-
-          <Button
-            className="hover:bg-primary/45"
-            size="md"
-            text="Get Demo"
-            variant="outline"
-          />
-        </div>
-      </div>
-    </nav>
+      </nav>
+    </SessionProvider>
   );
 }
--- apps/web/src/lib/auth.ts
+++ apps/web/src/lib/auth.ts
@@ -67,8 +67,8 @@ export const authOptions: NextAuthOptions = {
     }),
 
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID || "",
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
       authorization: {
         params: {
           prompt: "select_account",
@@ -77,8 +77,8 @@ export const authOptions: NextAuthOptions = {
     }),
 
     GitHubProvider({
-      clientId: process.env.GITHUB_ID || "",
-      clientSecret: process.env.GITHUB_SECRET || "",
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
     }),
   ],
 
