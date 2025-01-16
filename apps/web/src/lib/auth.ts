8a1a193cc12f1aebdd9e22bb826f868bd0a5a701
Abhoy Sarkar <sarkar.ab07@gmail.com>
2025-01-16 10:35:22 +0530

    sessionprovider added

--- apps/web/src/lib/auth.ts
+++ apps/web/src/lib/auth.ts
@@ -67,8 +67,8 @@ export const authOptions: NextAuthOptions = {
     }),
 
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
       authorization: {
         params: {
           prompt: "select_account",
@@ -77,8 +77,8 @@ export const authOptions: NextAuthOptions = {
     }),
 
     GitHubProvider({
-      clientId: process.env.GITHUB_ID as string,
-      clientSecret: process.env.GITHUB_SECRET as string,
+      clientId: process.env.GITHUB_ID ?? "",
+      clientSecret: process.env.GITHUB_SECRET ?? "",
     }),
   ],
 
@@ -184,5 +184,5 @@ export const authOptions: NextAuthOptions = {
     strategy: "jwt",
     maxAge: 24 * 60 * 60,
   },
-  secret: process.env.NEXTAUTH_SECRET || "",
+  secret: process.env.NEXTAUTH_SECRET ?? "",
 };
